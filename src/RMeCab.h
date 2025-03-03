00e6930abfb5c3e07fac24ba19ca5ed24723262c
paithiov909 <am11.44@gmail.com>
2025-03-04 00:23:04 +0900

    patch

--- src/RMeCab.h
+++ src/RMeCab.h
@@ -7,43 +7,44 @@
 　　　　 　 : 　,>　｀´￣｀´　<　 ′
 .　　　　 　 Ｖ　 　 　 　 　 　 Ｖ
 .　　　　 　 i{　●　 　 　 ●　}i
-　　　　 　 八　 　 ､_,_, 　 　 八 　　　わけがわからないよ 
+　　　　 　 八　 　 ､_,_, 　 　 八 　　　わけがわからないよ
 . 　 　 　 /　个 . ＿　 ＿ . 个 ',
 　　　＿/ 　 il 　 ,'　　　 '.　 li　 ',＿_
 
 
     docDF関数で全てをまかなえるから
-  
+
 */
 
-//#define WIN 0
-
+// #define WIN 0
 
 #ifndef _RMECAB_H
 #define _RMECAB_H
 
 #include "mecab.h"
-// #include "../inst/include/mecab.h"
+
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
 #include <R_ext/Riconv.h>
-#undef length 
-//#include <dirent.h>
-//#include <sys/types.h>
-//#include <sys/stat.h>
+#undef length
+
+#ifdef R_NO_REMAP
+#define allocVector Rf_allocVector
+#define lengthgets Rf_lengthgets
+#define mkCharCE Rf_mkCharCE
+#define xlength(x) Rf_xlength(x)
+#define ScalarInteger Rf_ScalarInteger
+#endif
 
 #include <string>
 #include <map>
 #include <list>
 #include <vector>
-// #include <functional>
-// #include <algorithm>
 #include <utility>
 #include <locale.h>
 
 using namespace std;
-
 
 /*
 vector <string> split(string str, string delim)
@@ -66,40 +67,37 @@ return result;
 }
 */
 
-
-
 // extern int utf8locale;
 
-
-
-extern  int BUF1 ;
-extern  int BUF2 ;
-extern  int BUF3 ;
-extern  int BUF4 ;
-extern  int FILEINPUT  ;
-extern  int OVERLINE  ;
-extern  int FILEN  ;
+extern int BUF1;
+extern int BUF2;
+extern int BUF3;
+extern int BUF4;
+extern int FILEINPUT;
+extern int OVERLINE;
+extern int FILEN;
 
 /////////////////////////////////////////////////////////////
 extern char meisi[128];
-extern char  * meisiCode();
+extern char* meisiCode();
 
 /////////////////////////////////////////////////////////////
 
 extern char keiyou[128];
-extern char  * keiyouCode(); 
+extern char* keiyouCode();
 ///////////////
 
 /////////////////////////////////////////////////////////////
 extern char kigo[128];
-extern char  * kigoCode();
+extern char* kigoCode();
 ////////////////////////////////////////////////
 
-
-#define CHECK(eval) if (! eval) { \
-    fprintf (stderr, "Exception:%s\n", mecab_strerror (mecab)); \
-    mecab_destroy(mecab); \
-    return (ScalarInteger(-1)); }
+#define CHECK(eval)                                           \
+  if (!eval) {                                                \
+    fprintf(stderr, "Exception:%s\n", mecab_strerror(mecab)); \
+    mecab_destroy(mecab);                                     \
+    return (ScalarInteger(-1));                               \
+  }
 ///////////////////////////////////////////////////////////////
 
 // extern "C" {
